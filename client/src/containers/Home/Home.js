539c51b4d54f7d1f3295bac8840ae439e4148761
Amina <amina.antoniazzi@gmail.com>
2020-08-01 16:22:19 +0100

    feat: add basic logic for homepage status update

--- client/src/containers/Home/Home.js
+++ client/src/containers/Home/Home.js
@@ -4,21 +4,22 @@ import './Home.css';
 
 function Home({ shouldWater }) {
 
-  // {
-  //   (!isLoading) ?
-  //   <div className="app_container">
-  //     <EventList events={events}/>
-  //     <EventForm createEvent={createEvent}/>
-  //     </div>  
-  //   : <div className="spinner">
-  //     <Spinner />
-  //   </div>
-  // }
-
   return (
-    <div className="home">
-      <h1>Aloe there! 👋</h1>
-      <img src={greenImg} alt="intro img" style={{ width: '100%' }} />
+    <div className="Home">
+      {
+        (!shouldWater) ?
+          <div className="home-green">
+            <h1>Aloe there! 👋</h1>
+            <h2>All your plants are watered and happy!</h2>
+            <img src={greenImg} alt="intro img" style={{ width: '100%' }} />
+          </div>
+          : <div className="home-yellow">
+            <h1>Aloe there! 👋</h1>
+            <h2>Your plants need some love (and water!)</h2>
+            <img src={greenImg} alt="intro img" style={{ width: '100%' }} />
+          </div>
+
+      }
       <h1>"Like people, plants respond to extra attention."</h1>
     </div>
   )
