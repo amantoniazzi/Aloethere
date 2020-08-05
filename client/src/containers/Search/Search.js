673fbe970897bc258e3a3dcb25316cf026c0957b
Amina <amina.antoniazzi@gmail.com>
2020-08-05 13:21:59 +0100

    feat: desktop styling

--- client/src/containers/Search/Search.js
+++ client/src/containers/Search/Search.js
@@ -8,12 +8,13 @@ function Search({ plants, filterPlants, shouldWater, emptyFilter }) {
   return (
     <div className="search_dashboard">
 
-      <div className="dashboard_filter">
-        {plants.length > 0
-          ? <p></p>
-          : <Filter plants={plants} filterPlants={filterPlants} shouldWater={shouldWater} />
-        }
-      </div>
+      {plants.length > 0
+        ? <p></p>
+        : <div className="dashboard_filter">
+          <Filter plants={plants} filterPlants={filterPlants} shouldWater={shouldWater} />
+        </div>
+      }
+
       <div className="dashboard_plantlist">
         <PlantList
           plants={plants}
