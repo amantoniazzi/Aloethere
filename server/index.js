e570178b6447d1268c18b26934802be3146c7555
Amina <amina.antoniazzi@gmail.com>
2020-08-16 09:44:42 +0100

    feat: ready for deploying

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const morgan = require('morgan');
 const router = require('./router');
 //const fs = require('fs');
